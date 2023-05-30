44375d990d7f3441d69aa3292d1ada98fba4ec97
Takerdat23 <87708802+Takerdat23@users.noreply.github.com>
2023-05-30 20:44:06 +0700

    First commit

---
+++ App.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using Wpf_Karaokay.Model;
+using System.Windows.Navigation;
+using Wpf_Karaokay.ViewModel; 
+
+namespace Wpf_Karaokay
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+  
+    public partial class App : Application
+    {
+        public TimerService TimerService { get; private set; }
+     
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            // Initialize the TimerService
+            TimerService = new TimerService();
+
+          
+
+
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# KaraOkay
-
 # SE104-Final-Project
 
 In this project, we will build an Karaoke manager desktop app with C#.NET 
+
+
